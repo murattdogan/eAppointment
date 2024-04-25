4a48320517ab272b72269b51879ae98660103ed2
murattdogan <muratdogan@hitit.edu.tr>
2024-04-25 17:28:37 +0300

    Add project files.

---
+++ eAppointment.Server.Domain/Entities/Doctor.cs
@@ -0,0 +1,18 @@
+﻿namespace eAppointment.Server.Domain.Entities
+{
+    public sealed class Doctor
+    {
+        public Doctor()
+        {
+            Id= Guid.NewGuid();
+        }
+
+        public Guid Id  { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName => string.Join(" ", FirstName, LastName);
+       
+    }
+}
